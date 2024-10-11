05dd74a5a5e792fdee506a00c9310c045a5ad55b
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-10-11 08:58:34 +0200

    Remove routes and db.js

--- backend/index.js
+++ backend/index.js
@@ -11,9 +11,10 @@ const user = require("./global");
 const connection = mysql.createConnection({
     host: '127.0.0.1', // Change this if your database is hosted elsewhere
     user: 'root', // Replace with your MySQL username
-    password: 'Emile1234!!', // Replace with your MySQL password
+    password: process.env.PASSWORD, // Replace with your MySQL password
     database: 'FinanceTrack' // The database you created
 });
+
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err.stack);
@@ -29,6 +30,7 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }));
+
 app.get("/test", async (req, res) => {
     connection.query('SELECT * FROM `User`', (err, results) => {
         if (err) {
@@ -37,74 +39,6 @@ app.get("/test", async (req, res) => {
         }
         console.log('Users:', results);
     });
-})
-app.get("/dashboard", async (req, res) => {
-    try {
-        connection.query(
-            'SELECT * FROM [User]',
-            function (err, results, fields) {
-                if (result.recordset.length > 0) {
-                    // const match = bcrypt.compare(req.body.password, result.recordset[0].password);
-                    if (true) {
-                        const u = results.recordset[0];
-
-                        user.username = u.username;
-                        user.firstName = u.firstName;
-                        user.lastName = u.lastName;
-                        user.dateJoined = u.dateJoined;
-
-                        req.session.user = user;
-                        localStorage.setItem("user", JSON.stringify(user));
-                        res.status(200).json(user);
-                    }
-                    else {
-                        res.status(401).json({ message: "Incorrect username or" })
-                    }
-                } else {
-                    res.status(401).json({ message: "Incorrect username or password" })
-                }
-            }
-        );
-
-
-        console.log(results); // results contains rows returned by server
-        console.log(fields); // fields contains extra meta data about results, if available
-    } catch (err) {
-        console.log(err);
-    }
-
-})
-app.post('/login', async (req, res) => {
-    try {
-        const pool = await connectToDatabase();
-        const result = await pool.request()
-            .input('id', sql.Int, req.body.username)
-            .query('SELECT * FROM [User] WHERE id = @id');
-
-        if (result.recordset.length > 0) {
-            const match = bcrypt.compare(req.body.password, result.recordset[0].password);
-            if (match) {
-                const u = result.recordset[0];
-
-                user.username = u.username;
-                user.firstName = u.firstName;
-                user.lastName = u.lastName;
-                user.dateJoined = u.dateJoined;
-
-                req.session.user = user;
-                localStorage.setItem("user", JSON.stringify(user));
-                res.status(200).json(user);
-            }
-            else {
-                res.status(401).json({ message: "Incorrect username or" })
-            }
-        } else {
-            res.status(401).json({ message: "Incorrect username or password" })
-        }
-    } catch (err) {
-        console.error('Error querying the database:', err);
-        res.status(500).json({ message: 'Error querying user' });
-    }
 });
 
 app.listen(port, () => { console.log(`Server live on port ${port}`) })
