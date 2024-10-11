6be5c6ec0cdd9644864a35e435df2db47dd4400f
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-10-11 19:33:19 +0200

    button component and page styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import Sidebar from './components/navigation/sidebar'
+import Button from './components/button/Button'
 
 function App() {
   return (
     <>
       <Sidebar />
-      <h1>Home</h1>
+      <main>
+        <h1>Good afternoon, Alex</h1>
+        <Button onClick={() => alert("Hi!")} className='primary-btn'>+ Add new asset</Button>
+      </main>
+
     </>
   )
 }
---
+++ frontend/src/components/button/Button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css'; // For styling
+
+const Button = ({ type = 'button', onClick, children, className = '', disabled = false }) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className={`btn ${className}`}
+            disabled={disabled}
+        >
+            {children}
+        </button>
+    );
+};
+
+export default Button;
