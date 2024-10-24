ebc4dc9e7bc31c9b260d614ea45675ee11d3af2e
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-10-24 17:27:34 +0200

    update max width for banks pie chart

--- frontend/src/pages/bank-accounts/BankPieChart.jsx
+++ frontend/src/pages/bank-accounts/BankPieChart.jsx
@@ -1,15 +1,21 @@
-import getColorShades from "../../utility/colors"
+import getColorShades from "../../utility/colors";
 import PieChart from "../../components/graph/PieChart";
 
 export default function BankPieChart({ accounts }) {
     const colorShades = getColorShades(accounts.length);
     const labels = [];
     const data = [];
-    accounts.forEach(a => {
+    accounts.forEach((a) => {
         labels.push(a.name);
         data.push(a.balance);
     });
     return (
-        <PieChart backgroundColors={colorShades} labels={labels} data={data} title={"Bank accounts"} />
-    )
+        <PieChart
+            backgroundColors={colorShades}
+            labels={labels}
+            data={data}
+            title={"Bank accounts"}
+            maxWidth={"500px"}
+        />
+    );
 }
