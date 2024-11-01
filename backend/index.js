41155d2bdbe98fcf69aac5ccc6934b94f29a1509
Alex van den Boogaard <alex.vandenboogaard@outlook..com>
2024-11-01 11:22:27 +0200

    make backend route for creating an account

--- backend/index.js
+++ backend/index.js
@@ -47,9 +47,6 @@ app.use("/savings/goal", saving_goals);
 app.use("/budget", budget);
 app.use("/statements", statements);
 
-app.get("/userid", (req, res) => {
-    res.status(200).json(req.session.userId);
-});
 app.post("/login", async (req, res) => {
     const { username, password } = req.body;
     const query =
@@ -70,6 +67,56 @@ app.post("/login", async (req, res) => {
         req.session.userId = results[0].id;
         req.session.loggedIn = true;
         res.status(200).json({ message: "Logged in" });
+    });
+});
+
+const saltRounds = 10;
+app.post("/create-account", async (req, res) => {
+    const { username, password, firstName, lastName } = req.body;
+
+    // Validate the input fields
+    if (!username || !password || !firstName) {
+        return res.status(400).json({ message: "Required fields missing" });
+    }
+
+    // Check if username is already taken
+    const checkUsernameQuery = "SELECT id FROM `User` WHERE username = ?";
+    connection.query(checkUsernameQuery, [username], async (err, results) => {
+        if (err) {
+            return res
+                .status(500)
+                .json({ message: "Error reading from the database" });
+        }
+        if (results.length > 0) {
+            return res.status(409).json({ message: "Username already exists" });
+        }
+
+        // Hash the password before saving to the database
+        try {
+            const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+            // Insert the new user into the database
+            const insertUserQuery = `
+                INSERT INTO 'User' (username, password, first_name, last_name)
+                VALUES (?, ?, ?, ?)
+            `;
+            connection.query(
+                insertUserQuery,
+                [username, hashedPassword, firstName, lastName || null],
+                (err, results) => {
+                    if (err) {
+                        return res
+                            .status(500)
+                            .json({ message: "Error creating the account" });
+                    }
+                    res.status(201).json({
+                        message: "Account created successfully",
+                    });
+                }
+            );
+        } catch (hashErr) {
+            res.status(500).json({ message: "Error encrypting password" });
+        }
     });
 });
 
