f70d0f3b30cc2bb3d51a33892995c22c90887449
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-11-09 17:01:18 +0200

    change widgets frontent

--- frontend/src/components/widget/Widget.jsx
+++ frontend/src/components/widget/Widget.jsx
@@ -1,9 +1,12 @@
-import "./Widget.css"
+import "./Widget.css";
 
-export default function Widget({ width, maxHeight, children }) {
+export default function Widget({ width, maxHeight = "500px", children }) {
     return (
-        <div className="widget" style={{ maxHeight: maxHeight, width: width }}>
+        <div
+            className="widget"
+            style={{ maxHeight: maxHeight, width: width, height: "100%" }}
+        >
             {children}
         </div>
-    )
+    );
 }
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -14,47 +14,70 @@ export default function Home() {
     const [assets, setAssets] = useState([]);
 
     // Calculate totals using useMemo
-    const totalBankAccounts = useMemo(() =>
-        bankAccounts.reduce((acc, a) => acc + parseFloat(a.balance || 0), 0),
+    const totalBankAccounts = useMemo(
+        () =>
+            bankAccounts.reduce(
+                (acc, a) => acc + parseFloat(a.balance || 0),
+                0
+            ),
         [bankAccounts]
     );
 
-    const totalInvestments = useMemo(() =>
-        investments.reduce((acc, i) => acc + parseFloat(i.currentValue || 0), 0),
+    const totalInvestments = useMemo(
+        () =>
+            investments.reduce(
+                (acc, i) => acc + parseFloat(i.currentValue || 0),
+                0
+            ),
         [investments]
     );
 
-    const totalAssets = useMemo(() =>
-        assets.reduce((acc, a) => acc + parseFloat(a.currentValue || 0), 0),
+    const totalAssets = useMemo(
+        () =>
+            assets.reduce((acc, a) => acc + parseFloat(a.currentValue || 0), 0),
         [assets]
     );
 
     const currentHour = new Date().getHours();
-    const greeting = currentHour < 12 ? "Good morning" : currentHour < 18 ? "Good afternoon" : "Good evening";
+    const greeting =
+        currentHour < 12
+            ? "Good morning"
+            : currentHour < 18
+            ? "Good afternoon"
+            : "Good evening";
 
     useEffect(() => {
-        fetchData("http://localhost:3001/overview")
-            .then((data) => {
-                setBankAccounts(data.bankAccounts);
-                setInvestments(data.investments);
-                setAssets(data.assets);
-            })
+        fetchData("http://localhost:3001/overview").then((data) => {
+            setBankAccounts(data.bankAccounts);
+            setInvestments(data.investments);
+            setAssets(data.assets);
+        });
     }, []);
 
     return (
         <main>
             <h1>{greeting}, Alex</h1>
-            <div style={{ display: "flex", flexWrap: "wrap", alignItems: "flex-start" }}>
-                <Widget width="500px" maxHeight={"auto"}>
-                    <NetWorthPie assets={assets} investments={investments} bankAccounts={bankAccounts} />
+            <div
+                style={{
+                    display: "flex",
+                    flexWrap: "wrap",
+                    alignItems: "flex-start",
+                }}
+            >
+                <Widget width="500px" maxHeight={"500px"}>
+                    <NetWorthPie
+                        assets={assets}
+                        investments={investments}
+                        bankAccounts={bankAccounts}
+                    />
                 </Widget>
-                <Widget width="500px" maxHeight="400px">
+                <Widget width="500px" maxHeight="500px">
                     <h2>Bank accounts: R{totalBankAccounts}</h2>
                     {bankAccounts.map((account, index) => (
                         <BankAccountDiv key={index} account={account} />
                     ))}
                 </Widget>
-                <Widget width="500px" maxHeight="400px">
+                <Widget width="500px" maxHeight="500px">
                     <h2>Investments: R{totalInvestments}</h2>
                     {investments.map((inv, index) => (
                         <InvestmentDiv key={index} investment={inv} />
