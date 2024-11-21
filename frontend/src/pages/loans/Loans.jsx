58c3abe72bd5145d441618971028f3492a379d1a
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-11-21 21:47:59 +0200

    make upcoming payments section

--- frontend/src/pages/loans/Loans.jsx
+++ frontend/src/pages/loans/Loans.jsx
@@ -5,6 +5,7 @@ import NewLoan from "./NewLoan";
 import LoansList from "./LoansList";
 import { fetchData } from "../../utility/fetchData";
 import "./Loans.css";
+import UpcomingPayment from "./UpcomingPayment";
 export default function Loans() {
     const [loans, setLoans] = useState([]);
     const [newLoanOpen, setNewLoanOpen] = useState(false);
@@ -51,6 +52,22 @@ export default function Loans() {
                 </Modal>
             </div>
             <LoansList loans={loans}></LoansList>
+            <h2 className="h2">Upcoming payments</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Balance</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {loans.map((loan) => (
+                        <UpcomingPayment key={loan.id} loan={loan} />
+                    ))}
+                </tbody>
+            </table>
         </main>
     );
 }
---
+++ frontend/src/pages/loans/UpcomingPayment.jsx
@@ -0,0 +1,21 @@
+export default function UpcomingPayment({ loan }) {
+    const getLastDayOfMonth = () => {
+        const today = new Date();
+        const lastDay = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+
+        const day = lastDay.getDate();
+        const month = lastDay.toLocaleString("default", { month: "long" });
+        const year = lastDay.getFullYear();
+
+        return `${day} ${month} ${year}`;
+    };
+
+    const day = getLastDayOfMonth();
+    return (
+        <tr>
+            <td>{loan.name}</td>
+            <td>{day}</td>
+            <td>{loan.monthly_repayment}</td>
+        </tr>
+    );
+}
