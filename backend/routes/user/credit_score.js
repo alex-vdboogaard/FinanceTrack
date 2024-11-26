1ca47448bbc4522137b3c6a08034dcce377d6784
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-11-26 15:41:12 +0200

    finish backend routes for credit score history

--- backend/routes/user/credit_score.js
+++ backend/routes/user/credit_score.js
@@ -6,22 +6,22 @@ const router = express.Router();
 router.use(ValidateLoggedIn);
 
 router.get("/", async (req, res) => {
-    const query = `SELECT TOP 1 * FROM Credit_Score WHERE user_id = ${req.session.userId} ORDER BY YEAR DESC, MONTH DESC`;
-    connection.query(query, (err, notifications) => {
+    const query = `SELECT * FROM Credit_Score_History WHERE user_id = ${req.session.userId} ORDER BY YEAR DESC, MONTH DESC LIMIT 1`;
+    connection.query(query, (err, credit_score) => {
         if (err) {
             res.status(500).json({
-                message: "Error reading from the database",
+                message: "Error reading from the database" + err.message,
             });
             return;
         }
 
-        res.status(200).json({ notifications });
+        res.status(200).json({ credit_score });
     });
 });
 
 router.post("/", (req, res) => {
-    const { year, month, score } = req.body;
-    const query = `INSERT INTO Credit_Score (year, month, score) VALUES (${year}, ${month}, ${score})`;
+    const { year, month, score, notes = "" } = req.body;
+    const query = `INSERT INTO Credit_Score (year, month, score, notes) VALUES (${year}, ${month}, ${score}, ${notes})`;
     connection.query(query, (err, results) => {
         if (err) {
             res.status(500).json({
