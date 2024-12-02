8c0757cfcdbbf0cb8bec96949a9fe89bd32361f2
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-12-02 12:08:54 +0200

    Make basic filtered folders component

---
+++ frontend/src/components/filtered-folders/FilteredFolders.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import fetchData from "../../utility/fetchData";
+import Folders from "../../pages/statements/folders/Folders";
+export default function FilteredFolders({ title = "", name }) {
+    const [folders, setFolders] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setLoading(true);
+        fetchData(`http://localhost:3001/statements/tag/${name}`).then(
+            (data) => {
+                setLoading(false);
+                setFolders(data.folders);
+            }
+        );
+    }, []);
+
+    return (
+        <>
+            <h2 className="h2">{title}</h2>
+            <Folders folders={folders} loading={loading}></Folders>;
+        </>
+    );
+}
