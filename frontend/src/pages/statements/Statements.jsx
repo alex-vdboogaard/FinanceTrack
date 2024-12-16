714971bcd00ecbf38e5761089588edeba5a03e66
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-12-16 09:00:36 +0200

    add prettier formatter

--- frontend/src/pages/statements/Statements.jsx
+++ frontend/src/pages/statements/Statements.jsx
@@ -12,150 +12,149 @@ import StatementsList from "./StatementsList.jsx";
 import SearchStatement from "./SearchStatement.jsx";
 
 const Statements = () => {
-    // State for files and folder data
-    const [folders, setFolders] = useState([]);
-    const [statements, setStatements] = useState([]);
-    const [recentFiles, setRecentFiles] = useState([]);
+  // State for files and folder data
+  const [folders, setFolders] = useState([]);
+  const [statements, setStatements] = useState([]);
+  const [recentFiles, setRecentFiles] = useState([]);
 
-    // Rerender states for each section
-    const [rerender, setRerender] = useState({
-        statements: false,
-        folders: false,
-        recentFiles: false,
-    });
+  // Rerender states for each section
+  const [rerender, setRerender] = useState({
+    statements: false,
+    folders: false,
+    recentFiles: false,
+  });
 
-    // Pagination states
-    const [currentPage, setCurrentPage] = useState(1);
-    const [numPages, setNumPages] = useState(1);
+  // Pagination states
+  const [currentPage, setCurrentPage] = useState(1);
+  const [numPages, setNumPages] = useState(1);
 
-    // Combined loading state object
-    const [loading, setLoading] = useState({
-        statements: true,
-        folders: true,
-        recentFiles: true,
-    });
+  // Combined loading state object
+  const [loading, setLoading] = useState({
+    statements: true,
+    folders: true,
+    recentFiles: true,
+  });
 
-    const handlePageChange = (page) => {
-        setCurrentPage(page);
-    };
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
 
-    useEffect(() => {
-        setLoading((prev) => ({ ...prev, statements: true }));
-        fetchData(`http://localhost:3001/statements?page=${currentPage}`).then(
-            (data) => {
-                setLoading((prev) => ({ ...prev, statements: false }));
-                setStatements(data.statements);
-                setNumPages(data.totalPages);
-                setCurrentPage(data.currentPage);
-            }
-        );
-    }, [currentPage, rerender.statements]);
-
-    useEffect(() => {
-        setLoading((prev) => ({ ...prev, folders: true }));
-        fetchData(`http://localhost:3001/statements/folders`).then((data) => {
-            setLoading((prev) => ({ ...prev, folders: false }));
-            setFolders(data.folders);
-        });
-    }, [rerender.folders]);
-
-    useEffect(() => {
-        setLoading((prev) => ({ ...prev, recentFiles: true }));
-        fetchData(`http://localhost:3001/statements/recent-files`).then(
-            (data) => {
-                setLoading((prev) => ({ ...prev, recentFiles: false }));
-                setRecentFiles(data.recentFiles);
-            }
-        );
-    }, [rerender.recentFiles]);
-
-    // Handle PDF preview
-    const handlePreview = (pdfBuffer) => {
-        try {
-            const uint8Array = new Uint8Array(pdfBuffer.data);
-            const blob = new Blob([uint8Array], { type: "application/pdf" });
-            const url = URL.createObjectURL(blob);
-            const newTab = window.open(url, "_blank");
-            if (newTab) {
-                newTab.onload = () => {
-                    URL.revokeObjectURL(url);
-                };
-            }
-        } catch (error) {
-            console.error("Error opening PDF preview:", error);
-        }
-    };
-
-    const handleDelete = async (id) => {
-        const confirm = await pops.confirmPop(
-            "Are you sure you want to delete this file?"
-        );
-        if (confirm) {
-            fetchData(`http://localhost:3001/statements`, "DELETE", {
-                id,
-            }).then((successData) => {
-                setStatements((prevStatements) =>
-                    prevStatements.filter((statement) => statement.id !== id)
-                );
-            });
-        }
-    };
-
-    const handleRerender = (section) => {
-        if (section === "all") {
-            // Toggle all sections
-            setRerender((prev) => {
-                const newRerender = {};
-                for (const key in prev) {
-                    newRerender[key] = !prev[key];
-                }
-                return newRerender;
-            });
-        } else {
-            // Toggle specific section
-            setRerender((prev) => ({ ...prev, [section]: !prev[section] }));
-        }
-    };
-
-    return (
-        <main>
-            <h1>Statements</h1>
-
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <h2 className="h2">Folders</h2>
-                <NewFolder
-                    rerender={() => handleRerender("folders")}
-                ></NewFolder>
-            </div>
-
-            <Folders
-                folders={folders}
-                rerender={() => handleRerender("all")}
-                loading={loading.folders}
-            ></Folders>
-
-            <RecentFiles
-                preview={handlePreview}
-                statements={recentFiles}
-                loading={loading.recentFiles}
-            />
-
-            <div style={{ display: "flex", alignItems: "center" }}>
-                <h2 className="h2">All files</h2>
-                <UploadStatement />
-            </div>
-            <SearchStatement></SearchStatement>
-            <StatementsList statements={statements} loading={loading}></StatementsList>
-
-            {numPages > 1 && (
-                <Pagination
-                    numPages={numPages}
-                    handleChange={handlePageChange}
-                    activePage={currentPage}
-                />
-            )}
-        </main>
+  useEffect(() => {
+    setLoading((prev) => ({ ...prev, statements: true }));
+    fetchData(`http://localhost:3001/statements?page=${currentPage}`).then(
+      (data) => {
+        setLoading((prev) => ({ ...prev, statements: false }));
+        setStatements(data.statements);
+        setNumPages(data.totalPages);
+        setCurrentPage(data.currentPage);
+      }
     );
+  }, [currentPage, rerender.statements]);
+
+  useEffect(() => {
+    setLoading((prev) => ({ ...prev, folders: true }));
+    fetchData(`http://localhost:3001/statements/folders`).then((data) => {
+      setLoading((prev) => ({ ...prev, folders: false }));
+      setFolders(data.folders);
+    });
+  }, [rerender.folders]);
+
+  useEffect(() => {
+    setLoading((prev) => ({ ...prev, recentFiles: true }));
+    fetchData(`http://localhost:3001/statements/recent-files`).then((data) => {
+      setLoading((prev) => ({ ...prev, recentFiles: false }));
+      setRecentFiles(data.recentFiles);
+    });
+  }, [rerender.recentFiles]);
+
+  // Handle PDF preview
+  const handlePreview = (pdfBuffer) => {
+    try {
+      const uint8Array = new Uint8Array(pdfBuffer.data);
+      const blob = new Blob([uint8Array], { type: "application/pdf" });
+      const url = URL.createObjectURL(blob);
+      const newTab = window.open(url, "_blank");
+      if (newTab) {
+        newTab.onload = () => {
+          URL.revokeObjectURL(url);
+        };
+      }
+    } catch (error) {
+      console.error("Error opening PDF preview:", error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    const confirm = await pops.confirmPop(
+      "Are you sure you want to delete this file?"
+    );
+    if (confirm) {
+      fetchData(`http://localhost:3001/statements`, "DELETE", {
+        id,
+      }).then((successData) => {
+        setStatements((prevStatements) =>
+          prevStatements.filter((statement) => statement.id !== id)
+        );
+      });
+    }
+  };
+
+  const handleRerender = (section) => {
+    if (section === "all") {
+      // Toggle all sections
+      setRerender((prev) => {
+        const newRerender = {};
+        for (const key in prev) {
+          newRerender[key] = !prev[key];
+        }
+        return newRerender;
+      });
+    } else {
+      // Toggle specific section
+      setRerender((prev) => ({ ...prev, [section]: !prev[section] }));
+    }
+  };
+
+  return (
+    <main>
+      <h1>Statements</h1>
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h2 className="h2">Folders</h2>
+        <NewFolder rerender={() => handleRerender("folders")}></NewFolder>
+      </div>
+
+      <Folders
+        folders={folders}
+        rerender={() => handleRerender("all")}
+        loading={loading.folders}
+      ></Folders>
+
+      <RecentFiles
+        preview={handlePreview}
+        statements={recentFiles}
+        loading={loading.recentFiles}
+      />
+
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <h2 className="h2">All files</h2>
+        <UploadStatement />
+      </div>
+      <SearchStatement></SearchStatement>
+      <StatementsList
+        statements={statements}
+        loading={loading}
+      ></StatementsList>
+
+      {numPages > 1 && (
+        <Pagination
+          numPages={numPages}
+          handleChange={handlePageChange}
+          activePage={currentPage}
+        />
+      )}
+    </main>
+  );
 };
 
 export default Statements;
--- frontend/src/pages/statements/StatementsList.jsx
+++ frontend/src/pages/statements/StatementsList.jsx
@@ -1,106 +1,94 @@
 import pops from "pop-message";
 import { Link } from "react-router-dom";
 import { fetchData } from "../../utility/fetchData";
-export default function StatementsList({statements, loading}) {
-        // Handle PDF preview
-        const handlePreview = (pdfBuffer) => {
-            try {
-                const uint8Array = new Uint8Array(pdfBuffer.data);
-                const blob = new Blob([uint8Array], { type: "application/pdf" });
-                const url = URL.createObjectURL(blob);
-                const newTab = window.open(url, "_blank");
-                if (newTab) {
-                    newTab.onload = () => {
-                        URL.revokeObjectURL(url);
-                    };
-                }
-            } catch (error) {
-                console.error("Error opening PDF preview:", error);
-            }
+export default function StatementsList({ statements, loading }) {
+  // Handle PDF preview
+  const handlePreview = (pdfBuffer) => {
+    try {
+      const uint8Array = new Uint8Array(pdfBuffer.data);
+      const blob = new Blob([uint8Array], { type: "application/pdf" });
+      const url = URL.createObjectURL(blob);
+      const newTab = window.open(url, "_blank");
+      if (newTab) {
+        newTab.onload = () => {
+          URL.revokeObjectURL(url);
         };
-    
-        const handleDelete = async (id) => {
-            const confirm = await pops.confirmPop(
-                "Are you sure you want to delete this file?"
-            );
-            if (confirm) {
-                fetchData(`http://localhost:3001/statements`, "DELETE", {
-                    id,
-                }).then((successData) => {
-                    setStatements((prevStatements) =>
-                        prevStatements.filter((statement) => statement.id !== id)
-                    );
-                });
-            }
-        };
-    return (
-        <table className="statements-table">
-        <thead>
-            <tr>
-                <th>Filename</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            {loading.statements === true ? (
-                <tr>
-                    <td colSpan="2" className="text-center">
-                        Loading...
-                    </td>
-                </tr>
-            ) : (
-                statements.map((statement) => (
-                    <tr key={statement.id}>
-                        <td>
-                        <div className="file-td">
-  <img
-    className="file-icon"
-    src="./src/assets/file.svg"
-    alt="file icon"
-  />
-  <p>
-    {statement.name}
-    <span className="grey-link">
-      {statement.folder_id == null ? (
-        ""
-      ) : (
-        <>
-          <Link style={{display:"inline"}} to={`/statements/folder/${statement.folder_id}`}>
-          {" / "} {statement.folder_name}
-          </Link>
-        </>
-      )}
-    </span>
-  </p>
-</div>
+      }
+    } catch (error) {
+      console.error("Error opening PDF preview:", error);
+    }
+  };
 
-                        </td>
-                        <td>
-                            <button
-                                onClick={() =>
-                                    handlePreview(statement.pdf_blob)
-                                }
-                            >
-                                <img
-                                    src="../src/assets/view.svg"
-                                    alt="preview"
-                                />
-                            </button>
-                            <button
-                                onClick={() =>
-                                    handleDelete(statement.id)
-                                }
-                            >
-                                <img
-                                    src="../src/assets/delete.svg"
-                                    alt="delete icon"
-                                />
-                            </button>
-                        </td>
-                    </tr>
-                ))
-            )}
-        </tbody>
+  const handleDelete = async (id) => {
+    const confirm = await pops.confirmPop(
+      "Are you sure you want to delete this file?"
+    );
+    if (confirm) {
+      fetchData(`http://localhost:3001/statements`, "DELETE", {
+        id,
+      }).then((successData) => {
+        setStatements((prevStatements) =>
+          prevStatements.filter((statement) => statement.id !== id)
+        );
+      });
+    }
+  };
+  return (
+    <table className="statements-table">
+      <thead>
+        <tr>
+          <th>Filename</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {loading.statements === true ? (
+          <tr>
+            <td colSpan="2" className="text-center">
+              Loading...
+            </td>
+          </tr>
+        ) : (
+          statements.map((statement) => (
+            <tr key={statement.id}>
+              <td>
+                <div className="file-td">
+                  <img
+                    className="file-icon"
+                    src="./src/assets/file.svg"
+                    alt="file icon"
+                  />
+                  <p>
+                    {statement.name}
+                    <span className="grey-link">
+                      {statement.folder_id == null ? (
+                        ""
+                      ) : (
+                        <>
+                          <Link
+                            style={{ display: "inline" }}
+                            to={`/statements/folder/${statement.folder_id}`}
+                          >
+                            {" / "} {statement.folder_name}
+                          </Link>
+                        </>
+                      )}
+                    </span>
+                  </p>
+                </div>
+              </td>
+              <td>
+                <button onClick={() => handlePreview(statement.pdf_blob)}>
+                  <img src="../src/assets/view.svg" alt="preview" />
+                </button>
+                <button onClick={() => handleDelete(statement.id)}>
+                  <img src="../src/assets/delete.svg" alt="delete icon" />
+                </button>
+              </td>
+            </tr>
+          ))
+        )}
+      </tbody>
     </table>
-    )
+  );
 }
