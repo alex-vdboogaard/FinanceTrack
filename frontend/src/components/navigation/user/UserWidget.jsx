3890cbc16c5d88053db1736450c89e8988ce9118
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2024-12-16 10:25:06 +0200

    fix small styling bugs

--- frontend/src/components/navigation/user/UserWidget.jsx
+++ frontend/src/components/navigation/user/UserWidget.jsx
@@ -9,131 +9,125 @@ import Modal from "../../modal/Modal";
 import UserAccountModal from "./UserAccountModal";
 
 export default function UserWidget({
-    showUserWidget,
-    closeUserWidget,
-    user,
-    creditScore = {},
-    netWorth = {},
+  showUserWidget,
+  closeUserWidget,
+  user,
+  creditScore = {},
+  netWorth = {},
 }) {
-    const handleExpand = () => {
+  const handleExpand = () => {
+    closeUserWidget();
+    setUserModalOpen(true);
+  };
+
+  const handleCloseUserModal = () => {
+    setUserModalOpen(false);
+  };
+
+  const [isUserModalOpen, setUserModalOpen] = useState(false);
+
+  useEffect(() => {
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape") {
         closeUserWidget();
-        setUserModalOpen(true);
+      }
     };
-
-    const handleCloseUserModal = () => {
-        setUserModalOpen(false);
+    if (showUserWidget) {
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
     };
+  }, [showUserWidget, closeUserWidget]);
 
-    const [isUserModalOpen, setUserModalOpen] = useState(false);
+  const handleLogout = async () => {
+    fetchData("http://localhost:3001/logout", "POST").then(() => {
+      window.location.href = "/login";
+    });
+  };
 
-    useEffect(() => {
-        const handleEscapeKey = (event) => {
-            if (event.key === "Escape") {
-                closeUserWidget();
-            }
-        };
-        if (showUserWidget) {
-            document.addEventListener("keydown", handleEscapeKey);
-        }
-        return () => {
-            document.removeEventListener("keydown", handleEscapeKey);
-        };
-    }, [showUserWidget, closeUserWidget]);
+  const assets = netWorth.assets;
+  const bankAccounts = netWorth.bankAccounts;
+  const investments = netWorth.investments;
+  const total1 = useMemo(
+    () =>
+      (assets || []).reduce(
+        (acc, asset) => acc + parseFloat(asset.currentValue || 0),
+        0
+      ),
+    [assets]
+  );
 
-    const handleLogout = async () => {
-        fetchData("http://localhost:3001/logout", "POST").then(() => {
-            window.location.href = "/login";
-        });
-    };
+  const total2 = useMemo(
+    () =>
+      (bankAccounts || []).reduce(
+        (acc, account) => acc + parseFloat(account.balance || 0),
+        0
+      ),
+    [bankAccounts]
+  );
 
-    const assets = netWorth.assets;
-    const bankAccounts = netWorth.bankAccounts;
-    const investments = netWorth.investments;
-    const total1 = useMemo(
-        () =>
-            (assets || []).reduce(
-                (acc, asset) => acc + parseFloat(asset.currentValue || 0),
-                0
-            ),
-        [assets]
-    );
+  const total3 = useMemo(
+    () =>
+      (investments || []).reduce(
+        (acc, investment) => acc + parseFloat(investment.currentValue || 0),
+        0
+      ),
+    [investments]
+  );
 
-    const total2 = useMemo(
-        () =>
-            (bankAccounts || []).reduce(
-                (acc, account) => acc + parseFloat(account.balance || 0),
-                0
-            ),
-        [bankAccounts]
-    );
+  const totalNetWorth = total1 + total2 + total3;
 
-    const total3 = useMemo(
-        () =>
-            (investments || []).reduce(
-                (acc, investment) =>
-                    acc + parseFloat(investment.currentValue || 0),
-                0
-            ),
-        [investments]
-    );
-
-    const totalNetWorth = total1 + total2 + total3;
-
-    return (
-        <>
-            <Modal
-                toggleSidebar={handleCloseUserModal}
-                type={"center"}
-                isOpen={isUserModalOpen}
-            >
-                <UserAccountModal
-                    user={user}
-                    creditScore={creditScore}
-                    netWorth={netWorth}
-                />
-            </Modal>
-            {showUserWidget && (
-                <div className="user-widget">
-                    <button
-                        onClick={() => {
-                            closeUserWidget();
-                        }}
-                        className="close-icon"
-                    >
-                        <img src={closeIcon} alt="close icon" />
-                    </button>
-                    <h3 className="tasks-title">My Account</h3>
-                    <div className="user-card">
-                        <img
-                            src={user.profile_image || userIcon}
-                            alt="user profile"
-                        />
-                        <div>
-                            <p className="user-fullname">
-                                {user.first_name} {user.last_name}
-                            </p>
-                            <p className="user-grey-data">
-                                Credit score: {creditScore.score}
-                            </p>
-                            <p className="user-grey-data">
-                                Net worth: R{totalNetWorth}
-                            </p>
-                        </div>
-                        <img
-                            className="logout-icon"
-                            onClick={handleLogout}
-                            src={logOutIcon}
-                            alt="log out"
-                        />
-                        <img
-                            className="expand-icon"
-                            onClick={handleExpand}
-                            src={expandIcon}
-                            alt="expand icon"
-                        />
-                    </div>
-                </div>
-            )}
-        </>
-    );
+  return (
+    <>
+      <Modal
+        toggleSidebar={handleCloseUserModal}
+        type={"center"}
+        isOpen={isUserModalOpen}
+      >
+        <UserAccountModal
+          user={user}
+          creditScore={creditScore}
+          netWorth={netWorth}
+        />
+      </Modal>
+      {showUserWidget && (
+        <div className="user-widget">
+          <button
+            onClick={() => {
+              closeUserWidget();
+            }}
+            className="close-icon"
+          >
+            <img src={closeIcon} alt="close icon" />
+          </button>
+          <h3 className="tasks-title">My Account</h3>
+          <div className="user-card">
+            <img src={user.profile_image || userIcon} alt="user profile" />
+            <div>
+              <p className="user-fullname">
+                {user.first_name} {user.last_name}
+              </p>
+              <p className="user-grey-data">
+                Credit score: {creditScore.score || 0}
+              </p>
+              <p className="user-grey-data">Net worth: R{totalNetWorth}</p>
+            </div>
+            <img
+              className="logout-icon"
+              onClick={handleLogout}
+              src={logOutIcon}
+              alt="log out"
+            />
+            <img
+              className="expand-icon"
+              onClick={handleExpand}
+              src={expandIcon}
+              alt="expand icon"
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
