5b0ebfb7b358c91826f68eff80b464d00df0ebfe
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-01-03 13:37:11 +0200

    add basic summary widget

---
+++ frontend/src/pages/tax/Summary.jsx
@@ -0,0 +1,120 @@
+import { useEffect, useState } from "react";
+import { getCurrentMonthInfo } from "../../utility/dates";
+import deleteIcon from "../../assets/delete.svg";
+import pops from "pop-message";
+import { fetchData } from "../../utility/fetchData";
+export default function Summary() {
+  const year = getCurrentMonthInfo().year;
+  const [items, setItems] = useState([
+    { name: "Salary", amount: 13000, due: 2000 },
+  ]);
+  const calculateTotals = (items) => {
+    const totalDue = items.reduce((acc, item) => acc + parseFloat(item.due), 0);
+    setTotalOwed(totalDue);
+  };
+
+  const [totalOwed, setTotalOwed] = useState(0);
+
+  const url = "http://localhost:3001/tax/summary";
+  const handleSave = (item) => {
+    fetchData(url, "PUT", item);
+  };
+  const handleDelete = async (asset) => {
+    const confirm = await pops.confirmPop(
+      `Are you sure you want to delete '${asset.name}'?`
+    );
+    if (confirm) {
+      fetchData(url, "DELETE", { id: asset.id, userId: asset.userId }).then(
+        () => {
+          const updatedItems = items.filter((item) => item.id !== item.id);
+          setItems(updatedItems);
+          calculateTotals(updatedItems);
+          pops.simplePop("success", "Item deleted");
+        }
+      );
+    }
+  };
+
+  const handleUpdate = (index, field, value) => {
+    const updatedItems = [...items];
+    updatedItems[index][field] = value;
+    setItems(updatedItems);
+    calculateTotals(updatedAssets);
+  };
+
+  useEffect(() => {
+    fetchData(url).then((data) => {
+      setItems(data.items);
+    });
+  }, []);
+
+  return (
+    <div className="summary-wrapper">
+      <h2 className="h2">Summary for {year}</h2>
+      <table>
+        <thead>
+          <tr className="no-border">
+            <th>Name</th>
+            <th>Amount (R)</th>
+            <th>Due to SARS (R)</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.map((item, index) => (
+            <tr key={index}>
+              <td>
+                <input
+                  type="text"
+                  value={item.name}
+                  onChange={(e) =>
+                    handleUpdate(index, "name", e.target.value, item)
+                  }
+                  onBlur={(e) => handleSave(item)}
+                />
+              </td>
+              <td>
+                <input
+                  type="number"
+                  value={item.amount}
+                  onChange={(e) =>
+                    handleUpdate(
+                      index,
+                      "amount",
+                      parseFloat(e.target.value),
+                      item
+                    )
+                  }
+                  onBlur={() => handleSave(item)}
+                />
+              </td>
+              <td>
+                <span>{item.due >= 0 ? "+ " : "- "}</span>
+                <input
+                  type="number"
+                  value={item.due}
+                  onChange={(e) =>
+                    handleUpdate(index, "due", parseFloat(e.target.value), item)
+                  }
+                  onBlur={() => handleSave(item)}
+                />
+              </td>
+              <td className="delete-icon">
+                <button onClick={() => handleDelete(item)}>
+                  <img src={deleteIcon} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan="2">Total</td>
+            <td>R{totalOwed}</td>
+            <td></td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
+}
--- frontend/src/pages/tax/Tax.jsx
+++ frontend/src/pages/tax/Tax.jsx
@@ -1,9 +1,12 @@
 import FilteredFolders from "../../components/filtered-folders/FilteredFolders";
+import Summary from "./Summary";
+import "./Tax.css";
 export default function Tax() {
   return (
     <main>
       <h1 className="h1">Tax</h1>
       <FilteredFolders title="Folders" name={"taxes"}></FilteredFolders>
+      <Summary></Summary>
     </main>
   );
 }
