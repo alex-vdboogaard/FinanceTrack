4cc98055b11b2ecdead9f44d05d972b95fb3270b
Alex van den Boogaard <alex.vandenboogaard@outlook.com>
2025-01-30 16:48:01 +0200

    fix modal children props display

--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -4,43 +4,43 @@ import closeIcon from "../../assets/close.svg";
 import { useEffect } from "react";
 
 const Modal = ({ isOpen, toggleSidebar, children, type = null }) => {
-    useEffect(() => {
-        const handleEscapeKey = (event) => {
-            if (event.key === "Escape" && isOpen) {
-                toggleSidebar();
-            }
-        };
+  useEffect(() => {
+    const handleEscapeKey = (event) => {
+      if (event.key === "Escape" && isOpen) {
+        toggleSidebar();
+      }
+    };
 
-        // Add event listener when the modal is open
-        if (isOpen) {
-            document.addEventListener("keydown", handleEscapeKey);
-        }
-
-        // Cleanup event listener when the modal is closed or unmounted
-        return () => {
-            document.removeEventListener("keydown", handleEscapeKey);
-        };
-    }, [isOpen, toggleSidebar]);
-
-    if (type === "center") {
-        return (
-            <div className={`center-modal ${isOpen ? "open" : ""}`}>
-                <button className="modal-toggle-right" onClick={toggleSidebar}>
-                    <img src={closeIcon} alt="close icon" />
-                </button>
-                <div>{children}</div>
-            </div>
-        );
-    } else {
-        return (
-            <div className={`modal ${isOpen ? "open" : ""}`}>
-                <button className="modal-toggle" onClick={toggleSidebar}>
-                    <img src={closeIcon} alt="close icon" />
-                </button>
-                {children}
-            </div>
-        );
+    // Add event listener when the modal is open
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscapeKey);
     }
+
+    // Cleanup event listener when the modal is closed or unmounted
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isOpen, toggleSidebar]);
+
+  if (type === "center") {
+    return (
+      <div className={`center-modal ${isOpen ? "open" : ""}`}>
+        <button className="modal-toggle-right" onClick={toggleSidebar}>
+          <img src={closeIcon} alt="close icon" />
+        </button>
+        {children}
+      </div>
+    );
+  } else {
+    return (
+      <div className={`modal ${isOpen ? "open" : ""}`}>
+        <button className="modal-toggle" onClick={toggleSidebar}>
+          <img src={closeIcon} alt="close icon" />
+        </button>
+        {children}
+      </div>
+    );
+  }
 };
 
 export default Modal;
